2ed800cf8306ac955ec4d7b185ef1041529dcaae
Erick Otenyo <otenyo.erick@gmail.com>
2026-05-12 05:46:26 +0000

    update staticfiles storage backend for prod

--- georiva/src/georiva/config/settings/production.py
+++ georiva/src/georiva/config/settings/production.py
@@ -6,7 +6,7 @@ DEBUG = False
 # outdated JavaScript / CSS assets being served from cache
 # (e.g. after a Wagtail upgrade).
 # See https://docs.djangoproject.com/en/5.2/ref/contrib/staticfiles/#manifeststaticfilesstorage
-STORAGES["staticfiles"]["BACKEND"] = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+STORAGES["staticfiles"]["BACKEND"] = "georiva.config.storage.ManifestStaticFilesStorageNotStrict"
 
 try:
     from .local import *
