d05d8a2b11e2f00126945e3a1aa3626217fcc3a2
Erick Otenyo <otenyo.erick@gmail.com>
2026-05-12 05:49:18 +0000

    Update ManifestStaticFilesStorageNotStrict

--- georiva/src/georiva/config/storage.py
+++ georiva/src/georiva/config/storage.py
@@ -2,4 +2,8 @@ from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
 
 
 class ManifestStaticFilesStorageNotStrict(ManifestStaticFilesStorage):
-    manifest_strict = False
+    def hashed_name(self, name, content=None, filename=None):
+        try:
+            return super().hashed_name(name, content=content, filename=filename)
+        except ValueError:
+            return name
